2d7ab8f0a8b4938fc8871dae434f51a637f39f33
dmarkovian <15883076+dmarkovian@users.noreply.github.com>
2024-11-10 15:18:26 -0800

    added stopwords data;

---
+++ backend/model.py
@@ -0,0 +1,40 @@
+import os
+import re
+import string
+import sys
+import time
+
+sys.path.append(os.path.join(os.path.abspath("."), "code"))
+
+#from plotting_functions_unsup import *
+
+import IPython
+import numpy as np
+import numpy.random as npr
+import pandas as pd
+import nltk
+import gensim
+import gensim.downloader as api
+
+
+#from comat import CooccurrenceMatrix
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize, word_tokenize
+#from preprocessing import MyPreprocessor
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+
+DATA_DIR = os.path.relpath("./data/")
+
+nltk.download('stopwords', download_dir=DATA_DIR)
+
+print(list(api.info()["models"].keys()))
+
+
+# Cosine Similarity toy func
+#cosine_similarity = np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+#print(f"Cosine Similarity: {cosine_similarity:.4f}")
+
+google_news_vectors = api.load('word2vec-google-news-300')
+print("Size of vocabulary: ", len(google_news_vectors))
